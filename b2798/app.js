6be39f91cf61a888365a168686f7bb24a0ee32fa
jinhobyeong <jhb_0921@naver.com>
2022-01-30 23:14:53 +0900

    basic 2/3

--- b2798/app.js
+++ b2798/app.js
@@ -8,26 +8,22 @@ solution(inputN, inputX, arrayA);
 
 function solution(n, num, arr) {
     let max = 0;
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < n; j++) {
-          for(let k = 0; k < n; k++) {
-            // 동일한 카드를 선택 할 수는 없기 때문에 아래 조건의 경우 다음 loop로 넘어간다.
-            if(i == j || i == k || j == k) {
-              continue;
+    for(let i = 0; i<n; i++){
+        for(let j = 0; j<n; j++){
+            for(let z = 0; z<n; z++){
+                let sum = 0;
+                if(i == z || j == z || i == j){
+
+                }
+                else{
+                    sum = arr[i] + arr[j] + arr[z];
+                    if(sum > max && sum <= num){
+                        max = sum;
+                    }
+                }
             }
-       
-            // 서로 다른 카드가 선택된 경우 M을 초과하지 않는 최대값인지 확인 후 저장한다.
-            const sum = arr[i] + arr[j] + arr[k]
-            if(sum > max && sum <= num) {
-              max = sum
-            }
-       
-            // 규칙에 맞는 숫자가 나온 경우에 더이상 계산을 할 필요가 없기 떄문에 종료한다.
-            if(max == num) {
-              break;
-            }
-          }
         }
-      }
-      console.log(max);
+    }
+
+    console.log(max);
 }
