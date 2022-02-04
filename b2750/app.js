e7932ba40fae0a0003e4551fcf18f0f4544db375
jinhobyeong <jhb_0921@naver.com>
2022-02-04 23:42:50 +0900

    2/3 basic

--- b2750/app.js
+++ b2750/app.js
@@ -1,35 +1,10 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-let N = Number(input.shift());
-let numArr = input.map(x => +x);
 
-solution(numArr);
+const N = input.shift();
+let results = '';
+input.sort((a, b) => a - b).forEach(num => (results += `${num}\n`));
+console.log(results);
 
-
-function solution(sortArr){
-    
-    for(let i = 1; i<sortArr.length; i++){ //0배열은 정렬되어있다고 가정하고 1인덱스 배열 부터 시작한다 따라서 i = 1이고 총 4번만 돌린다.
-        let curIndex = i - 1;
-        let currentNum = sortArr[i];
-
-        while(curIndex >=0 && sortArr[curIndex] > currentNum){
-            sortArr[curIndex + 1] = sortArr[curIndex];
-            curIndex--;
-        }
-
-        sortArr[curIndex+1] = currentNum;
-    }
-
-    console.log(sortArr);
-}
-
-
-// const input = [5, 5, 2, 3, 4, 1];
-// const N = input.shift();
-// const sorted = input.sort((a, b) => a - b);
- z1
-// for (let i = 0; i < N; i++) {
-// console.log(sorted[i]);
-// }
- 
+//(a,b) => (a-b)의미 : a,b는 함수(1,2,3) 이 있을 때 b에 2가 들어가고 a에 1이 들어가서 a-b했을 떄  b가 더 크게 된다면 음수가 나와서 변형이 일어나 젤 큰 것들이 앞으로 오게된다.
