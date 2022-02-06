0c818902501e7cf65ff296e45b67784658c39c81
jinhobyeong <jhb_0921@naver.com>
2022-02-06 23:52:34 +0900

    basic

--- basic/b2751/app.js
+++ basic/b2751/app.js
@@ -28,7 +28,7 @@ function solution(sortArr){
 // const input = [5, 5, 2, 3, 4, 1];
 // const N = input.shift();
 // const sorted = input.sort((a, b) => a - b);
- z1
+
 // for (let i = 0; i < N; i++) {
 // console.log(sorted[i]);
 // }
